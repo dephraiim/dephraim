de6151492708d39d8b1a24c0903df86b801cc7dc
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2020-09-07 01:33:43 +0000

    Add Readme

---
+++ readme.js
@@ -0,0 +1,21 @@
+/**
+ * Personal cli for bootstraping projects
+ * Ephraim Atta-Duncan
+ * MIT License
+ */
+
+/**
+ *
+ **BOILERPLATES TO ADD**
+ * React
+ * Vue
+ * React-Redux
+ * React-Router
+ * React-Router-Redux
+ * Vue-Router
+ * Vue-Vuex
+ * React-TypeScript
+ * TypeScript
+ * TypeScript Library
+ *
+ */
